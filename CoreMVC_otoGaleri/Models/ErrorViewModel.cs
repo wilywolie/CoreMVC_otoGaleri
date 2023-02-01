c34e3886b2e49315a80d18571172cb880d16f502
Ahmet AKDEMIR <ahmet.akdemir@st.uskudar.edu.tr>
2023-02-01 13:38:38 +0300

    Add project files.

---
+++ CoreMVC_otoGaleri/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreMVC_otoGaleri.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
